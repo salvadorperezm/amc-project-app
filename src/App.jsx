d6a81c98e3217993b1bb389c6e95c04480e9994c
salvadorperezm <salvadorperezm115@gmail.com>
2023-06-09 14:42:45 -0600

    Added chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import './App.css'
+import { SunIcon, UnlockIcon } from "@chakra-ui/icons";
+import { Box, Button } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
-  )
+    <Box
+      minH={"100vh"}
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <Button
+        size={"lg"}
+        mx={"10px"}
+        colorScheme={"yellow"}
+        color={"white"}
+        leftIcon={<SunIcon />}
+      >
+        Encender
+      </Button>
+      <Button
+        size={"lg"}
+        mx={"10px"}
+        colorScheme={"linkedin"}
+        color={"white"}
+        leftIcon={<UnlockIcon />}
+      >
+        Apagar
+      </Button>
+    </Box>
+  );
 }
 
-export default App
+export default App;
